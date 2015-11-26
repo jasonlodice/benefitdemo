b61d77be381636bdd17fa2c07b7dac99bc6f194d
Jason Lodice <jasonlodice@gmail.com>
2015-11-26 11:51:14 -0500

    scaffold app with yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name benefitdemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the benefitdemoApp
+ */
+angular.module('benefitdemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name benefitdemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the benefitdemoApp
+ */
+angular.module('benefitdemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
