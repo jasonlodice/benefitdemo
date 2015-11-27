c07d5e438b563158e9b59e95fef0908a7bc5b5f5
Jason Lodice <jasonlodice@gmail.com>
2015-11-27 16:56:21 -0500

    refactor paycheck calculation into calc service

--- src/web/app/scripts/controllers/employeeDetail.js
+++ src/web/app/scripts/controllers/employeeDetail.js
@@ -8,19 +8,10 @@
  * Controller of the benefitApp
  */
 angular.module('benefitApp')
-  .controller('EmployeeDetailCtrl', ['$routeParams', 'employeeService', 'paycheckConfigService',
-    function($routeParams, employeeService, paycheckConfigService) {
-
+  .controller('EmployeeDetailCtrl', ['$routeParams', 'employeeService', 'paycheckCalcService',
+    function($routeParams, employeeService, paycheckCalcService) {
       //	private fields
       var self = this;
-      var perPeriodEmpCost = paycheckConfigService.annualEmpBenefitCost / paycheckConfigService.payPeriods;
-      var perPeriodEmpDiscount = perPeriodEmpCost * paycheckConfigService.vipDiscountRate;
-      var perPeriodDependentCost = paycheckConfigService.annualDepBenefitCost / paycheckConfigService.payPeriods;
-      var perPeriodDependentDiscount = perPeriodDependentCost * paycheckConfigService.vipDiscountRate;
-
-      //	public fields
-      this.payPeriods = paycheckConfigService.payPeriods;
-      this.employeeCost = perPeriodEmpCost;
 
       //	fetch employee
       employeeService.get($routeParams.id)
@@ -29,26 +20,9 @@ angular.module('benefitApp')
           self.calculatePaycheck();
         });
 
-      //	determine if employee or dependent qualifies for discount
-      var qualifiesForVipDiscount = function(person) {
-        return person.first && person.first.length > 0 && person.first[0] === 'A';
-      }
-
       //	calculate called when employee detail changes
       this.calculatePaycheck = function() {
-        this.grossPay = this.employee.grossPay;
-        this.employeeDiscount = qualifiesForVipDiscount(this.employee) ? perPeriodEmpDiscount : 0;
-        this.dependentCost = this.employee.dependents.length * perPeriodDependentCost;
-
-				//	count vip dependents
-        var vipDependents = 0;
-        angular.forEach(this.employee.dependents, function(dep) {
-          if (qualifiesForVipDiscount(dep)) {
-            vipDependents++;
-          }
-        });
-
-        this.dependentDiscount = vipDependents * perPeriodDependentDiscount;
+				this.paycheck = paycheckCalcService.calculatePaycheck(this.employee);
       };
     }
   ]);
--- src/web/app/scripts/services/paycheckCalcService.js
+++ src/web/app/scripts/services/paycheckCalcService.js
@@ -8,6 +8,36 @@
  * Service in the benefitApp.
  */
 angular.module('benefitApp')
-  .service('paycheckCalcService', function () {
+  .service('paycheckCalcService', ['paycheckConfigService', function(paycheckConfigService) {
+    var perPeriodEmpCost = paycheckConfigService.annualEmpBenefitCost / paycheckConfigService.payPeriods;
+    var perPeriodEmpDiscount = perPeriodEmpCost * paycheckConfigService.vipDiscountRate;
+    var perPeriodDependentCost = paycheckConfigService.annualDepBenefitCost / paycheckConfigService.payPeriods;
+    var perPeriodDependentDiscount = perPeriodDependentCost * paycheckConfigService.vipDiscountRate;
 
-  });
+    //	determine if employee or dependent qualifies for discount
+    var qualifiesForVipDiscount = function(person) {
+      return person.first && person.first.length > 0 && person.first[0] === 'A';
+    }
+
+    //	calculate called when employee detail changes
+    this.calculatePaycheck = function(employee) {
+      var paycheck = {};
+
+			//	count vip dependents
+			var vipDependents = 0;
+			angular.forEach(employee.dependents, function(dep) {
+				if (qualifiesForVipDiscount(dep)) {
+					vipDependents++;
+				}
+			});
+
+			paycheck.payPeriods = paycheckConfigService.payPeriods;
+      paycheck.grossPay = employee.grossPay;
+			paycheck.employeeCost = perPeriodEmpCost;
+      paycheck.employeeDiscount = qualifiesForVipDiscount(employee) ? perPeriodEmpDiscount : 0;
+      paycheck.dependentCost = employee.dependents.length * perPeriodDependentCost;
+      paycheck.dependentDiscount = vipDependents * perPeriodDependentDiscount;
+      paycheck.netPay = paycheck.grossPay - paycheck.employeeCost + paycheck.employeeDiscount - paycheck.dependentCost + paycheck.dependentDiscount;
+			return paycheck;
+    };
+  }]);
