788cd16dba980d2aebc8a58af01929f619f9ec09
Jason Lodice <jasonlodice@gmail.com>
2015-11-28 21:55:12 -0500

    more whitespace changes

--- src/web/app/scripts/services/paycheckConfigService.js
+++ src/web/app/scripts/services/paycheckConfigService.js
@@ -8,10 +8,10 @@
  * Service in the benefitApp.
  */
 angular.module('benefitApp')
-  .service('paycheckConfigService', function() {
-    //	these values may be been fetched from an API endpoint
-    this.payPeriods = 26;
-    this.annualEmpBenefitCost = 1000;
-    this.annualDepBenefitCost = 500;
-    this.vipDiscountRate = 0.1;
+  .service('paycheckConfigService', function () {
+  	//	these values may be been fetched from an API endpoint
+  	this.payPeriods = 26;
+  	this.annualEmpBenefitCost = 1000;
+  	this.annualDepBenefitCost = 500;
+  	this.vipDiscountRate = 0.1;
   });
