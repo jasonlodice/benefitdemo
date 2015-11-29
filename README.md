8af55cf9dfd2a98e4a9fb55a8da4af46d4d2c63b
Jason Lodice <jasonlodice@gmail.com>
2015-11-29 15:30:24 -0500

    update README.md

--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.14.0.
 
+**Play with live version on** [Plunker](http://embed.plnkr.co/DOBU57oebpLMxX9dbdMq/)
+
 ## Build & development
 
 ### Front End
 
 * built as a single page application using AngularJS
+* Run `bower install` and `npm install`
 * Run `grunt` for building and `grunt serve` for preview.
+* Running `grunt test` will run the unit tests with karma.
 
 ### Back End
 
 * built using ASP.NET Web API 2
 * self hosted using OWIN
 * binds to http://+:80/lopay
-
-## Testing
-
-Running `grunt test` will run the unit tests with karma.
--- src/web/app/scripts/controllers/employeeDetail.js
+++ src/web/app/scripts/controllers/employeeDetail.js
@@ -9,78 +9,78 @@
  */
 angular.module('benefitApp')
   .controller('EmployeeDetailCtrl', ['$routeParams', '$location', 'employeeService', 'paycheckCalcService',
-    function ($routeParams, $location, employeeService, paycheckCalcService) {
-    	//	private fields
-    	var self = this;
+	function ($routeParams, $location, employeeService, paycheckCalcService) {
+		//	private fields
+		var self = this;
 
-    	//	public fields
-    	this.newDependent = {};
-    	this.editingDependent = false;
+		//	public fields
+		this.newDependent = {};
+		this.editingDependent = false;
 
-    	//	recalculate the paycheck
-    	this.calculatePaycheck = function () {
-    		this.paycheck = paycheckCalcService.calculatePaycheck(this.employee);
-    	};
+		//	recalculate the paycheck
+		this.calculatePaycheck = function () {
+			this.paycheck = paycheckCalcService.calculatePaycheck(this.employee);
+		};
 
-    	//	save a depdendent
-    	this.saveDependent = function () {
-    		//	save value
-    		this.employee.dependents.push(this.newDependent);
-    		this.save();
-    		this.cancelDependent();
-    	};
+		//	save a depdendent
+		this.saveDependent = function () {
+			//	save value
+			this.employee.dependents.push(this.newDependent);
+			this.save();
+			this.cancelDependent();
+		};
 
-    	//	delete a depdendent
-    	this.deleteDependent = function (dependent) {
-    		var index = this.employee.dependents.indexOf(dependent);
-    		if (index > -1) {
-    			this.employee.dependents.splice(index, 1);
-    		}
-    		this.save();
-    	};
+		//	delete a depdendent
+		this.deleteDependent = function (dependent) {
+			var index = this.employee.dependents.indexOf(dependent);
+			if (index > -1) {
+				this.employee.dependents.splice(index, 1);
+			}
+			this.save();
+		};
 
-    	//	save the employee and dependents
-    	this.save = function () {
-    		if (this.employee.id) {
-    			//	this is saving an existing employee
-    			employeeService.update(this.employee.id, this.employee)
+		//	save the employee and dependents
+		this.save = function () {
+			if (this.employee.id) {
+				//	this is saving an existing employee
+				employeeService.update(this.employee.id, this.employee)
 		.then(function (result) {
 			self.employee = result.data;
 			self.calculatePaycheck();
 		});
-    		} else {
-    			//	this is a first time save
-    			employeeService.create(this.employee)
+			} else {
+				//	this is a first time save
+				employeeService.create(this.employee)
 				  .then(function (result) {
-				  	//	reload the page with new id
-				  	$location.path('/employee/' + result.data.id);
+					//	reload the page with new id
+					$location.path('/employee/' + result.data.id);
 				  });
-    		}
-    	};
+			}
+		};
 
-    	//	begin editing a dependent
-    	this.startNewDependent = function () {
-    		this.editingDependent = true;
-    	};
+		//	begin editing a dependent
+		this.startNewDependent = function () {
+			this.editingDependent = true;
+		};
 
-    	//	finish editing a dependent
-    	this.cancelDependent = function () {
-    		//	reset the model
-    		this.newDependent = {};
-    		this.editingDependent = false;
-    	};
+		//	finish editing a dependent
+		this.cancelDependent = function () {
+			//	reset the model
+			this.newDependent = {};
+			this.editingDependent = false;
+		};
 
-    	//	initialize data
-    	if ($routeParams.id === 'new') {
-    		//	if new, get default values for form
-    		this.employee = employeeService.newDefault();
-    		this.calculatePaycheck();
-    	} else {
-    		//	fetch existing employee
-    		employeeService.get($routeParams.id)
+		//	initialize data
+		if ($routeParams.id === 'new') {
+			//	if new, get default values for form
+			this.employee = employeeService.newDefault();
+			this.calculatePaycheck();
+		} else {
+			//	fetch existing employee
+			employeeService.get($routeParams.id)
 			  .then(function (result) {
-			  	self.employee = result.data;
-			  	self.calculatePaycheck();
+				self.employee = result.data;
+				self.calculatePaycheck();
 			  });
-    	}
-    }]);
+		}
+	}]);
